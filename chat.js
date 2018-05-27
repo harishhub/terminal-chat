9724075d96a8218e9d7de9d9b479fa2652d0d7bd
harish <hk8049@gmail.com>
2018-05-27 23:44:00 +0530

    added server start log msgs and error msgs

--- chat.js
+++ chat.js
@@ -5,7 +5,7 @@ let sockets = [];
 let server = net.Server((socket) => {
     sockets.push(socket);
     socket.on('data', (d) => {
-        for (var i=0; i< sockets.length; i++) {
+        for (let i=0; i< sockets.length; i++) {
 
             if (sockets[i] == socket){
                 continue;
@@ -22,3 +22,40 @@ let server = net.Server((socket) => {
 });
 
 server.listen(8000);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    let addr = server.address();
+    let bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    console.log("Sever Listening On Port : " + addr.port);
+}
